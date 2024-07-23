792fb6ca598cd43c8983a19684f38ce9a444c781
Vincent Payet <vincentp9@outlook.fr>
2024-07-23 16:15:40 +0400

    récupération des valeurs email et password de l'utilisateur

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -2,3 +2,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('user-login-form').addEventListener('submit', function (event) {
         event.preventDefault();
+        // Récupération des valeurs entrées dans email et password
+        const user = {
+            email: document.querySelector('#email').value,
+            password: document.querySelector('#password').value,
+        };
