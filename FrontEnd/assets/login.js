9f02e4f69108e3fadb21908ff7d4526cf446c3cb
Vincent Payet <vincentp9@outlook.fr>
2024-07-23 16:05:55 +0400

    fonction pour éxécuter le code js quand la page est chargée

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,4 @@
+// Executing JS code when the page is loaded
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('user-login-form').addEventListener('submit', function (event) {
+        event.preventDefault();
